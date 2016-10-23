41620e77d9d176e33e3f562da6a96079c390c580
Gabriel Erbetta <gabrielerbetta@gmail.com>
2016-10-23 20:34:09 -0300

    Added discipline informations modal

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,23 +1,27 @@
 class DisciplinesController < ApplicationController
-  layout false, only: [:get_information]
+  layout false, only: [:get_information, :ajax_search]
 
   def get_information
-    @discipline = Discipline.find_by_code params[:code]
-    @course = Course.find_by_code params[:course]
+    @discipline = Discipline.includes(discipline_classes: { schedules: :professors }).find_by_code params[:code]
 
-    if @discipline and @course
-      @course_discipline = CourseDiscipline.where(discipline: @discipline, course: @course).first
+    if params[:course].present?
+      @course = Course.find_by_code params[:course]
 
-      if @course_discipline
-        @pre_requisites = @course_discipline.pre_requisites
-        @post_requisites = @course_discipline.post_requisites
+      course_discipline = CourseDiscipline.includes([
+        pre_requisites:  { pre_discipline:  :discipline },
+        post_requisites: { post_discipline: :discipline }
+      ]).where(discipline: @discipline, course: @course).first
 
-        @discipline_classes = @discipline.discipline_classes
+      if course_discipline
+        @pre_requisites = course_discipline.pre_requisites.map { |pre| pre.pre_discipline.discipline }
+        @post_requisites = course_discipline.post_requisites.map { |post| post.post_discipline.discipline }
       end
     end
+
+    @discipline_classes = @discipline.discipline_classes
   end
 
   def ajax_search
-    @disciplines = Discipline.where 'name ILIKE ? or code ILIKE ?', "%#{params[:pattern]}%", "%#{params[:pattern]}%"
+    @disciplines = Discipline.where 'unaccent(name) ILIKE unaccent(?) or code ILIKE ?', "%#{params[:pattern]}%", "%#{params[:pattern]}%"
   end
 end
